3c9c0aa5c15a08bbf603e7296992d53769225589
RainKey <ratmdms@gmail.com>
2020-03-02 00:44:57 +0900

    Edit BookCategoryRespository and Add RepositoryConfig

--- onlinebookstore/onlinebookstore/src/main/java/com/callmejay/onlinebookstore/repository/BookCategoryRepository.java
+++ onlinebookstore/onlinebookstore/src/main/java/com/callmejay/onlinebookstore/repository/BookCategoryRepository.java
@@ -1,11 +1,11 @@
 package com.callmejay.onlinebookstore.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.callmejay.onlinebookstore.entity.BookCategory;
 
-
-
+@RepositoryRestResource(collectionResourceRel="bookCategory", path="book-category")
 public interface BookCategoryRepository extends JpaRepository<BookCategory, Long>{
 
 }
